7f2859fceb5ffeda359252733735728dbf03ffa6
noprysk85 <nazariy.oprysk@gmail.com>
2023-03-17 09:15:45 -0600

    commit 3

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,10 +1,9 @@
 pipeline {
-    agent none
+    agent {
+        docker { image 'node:16.13.1-alpine' }
+    }
     stages {
         stage('Test') {
-            agent {
-                docker { image 'node:16.13.1-alpine' }
-            }
             steps {
                 sh 'node --version'
             }
